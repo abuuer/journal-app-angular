02d627d9eba4533f11c2bdf85f6c8b6d126e8e38
Abuuer <anouarabouerraja@gmail.com>
2020-06-04 11:56:19 +0000

    11 attempt heroku

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT
+const port = process.env.PORT || 8080
 
-app.use(express.static(__dirname + '/src'));
-
+app.use(express.static(__dirname + '/dist/review-app'));
 app.listen(port, () => {
   console.log(`listening on ${port}`)
 });
 
 app.get('/*', function (req,res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/review-app/index.html'));
 })
 

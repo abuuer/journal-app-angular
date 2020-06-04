0062a3a889fe56b5375881c824ee34f9d7a3a45c
Abuuer <anouarabouerraja@gmail.com>
2020-06-04 11:34:43 +0000

    10 attempt heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT
 
 app.use(express.static(__dirname + '/src'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+});
 
 app.get('/*', function (req,res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
-console.log('console listening')
